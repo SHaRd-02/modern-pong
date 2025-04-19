7725f301b3cc0ca3c488b53dc4379b6f37b817cc
Ricardo <ricaroherniag@gmail.com>
2025-04-19 03:31:50 -0700

    finished v0.8

--- modern pong.service.worker.js
+++ modern pong.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1745014926|58684438649';
+const CACHE_VERSION = '1745058599|102358693485';
 /** @type {string} */
 const CACHE_PREFIX = 'modern pong-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
